c45a25037e1ea1c404b93b6095385636b6ada130
Vegard Hagen <vegard@edgeworks.no>
2021-07-26 17:58:14 +0200

    feat: removed explicit test of application start. Application start is implicitly tested in integration tests.

--- src/main/kotlin/no/edgeworks/kotlinbeer/KotlinBeerApplication.kt
+++ src/main/kotlin/no/edgeworks/kotlinbeer/KotlinBeerApplication.kt
@@ -1,28 +1,13 @@
 package no.edgeworks.kotlinbeer
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.ConfigurableApplicationContext
 
 @SpringBootApplication
-class KotlinBeerApplication {
+class KotlinBeerApplication
 
-    companion object {
-        lateinit var applicationContext: ConfigurableApplicationContext
-
-        @JvmStatic
-        fun main(args: Array<String>) {
-            applicationContext = runApplication<KotlinBeerApplication>(*args)
-        }
-
-        @JvmStatic
-        fun stop() {
-            SpringApplication.exit(applicationContext)
-        }
-
-    }
+fun main(args: Array<String>) {
+    runApplication<KotlinBeerApplication>(*args)
 }
-
 
 
